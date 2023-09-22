b37b93fa35eb4b75f60258be6afae17684de657f
rejaul <devrejaul.official@gmail.com>
2023-09-23 01:06:35 +0600

    add

--- src/20.dynamic_slider_automation/Video.js
+++ src/20.dynamic_slider_automation/Video.js
@@ -4,27 +4,35 @@ import { Video, ResizeMode } from 'expo-av';
 import { useRef } from 'react';
 import { useEffect } from 'react';
 import { useSliderContext } from './AutoSlider';
+import VisibilitySensor from '@svanboxel/visibility-sensor-react-native'
 const {width, height} = Dimensions.get("screen")
 
 export default function VideoCom({item,position}) {
     const video = useRef(null);
    const {setStop} = useSliderContext()
 
+   const handleImageVisibility = visible => {
+    if (visible) {
+      setStop(true)
+    }
+  }
 
-   useEffect(()=>{
-    console.log(Math.ceil(position/width));
-   })
-
-    const handlePlayState = (s)=>{}
+    const handlePlayState = (videoState)=>{
+console.log(videoState.didJustFinish);
+    }
     
   return (
+    <VisibilitySensor onChange={handleImageVisibility}>
      <Video
         style={styles.img}
         ref={video}
         source={{ uri:item.uri}}
         resizeMode={ResizeMode.CONTAIN}
         onPlaybackStatusUpdate={handlePlayState}
+        shouldPlay
+        isLooping
     />
+    </VisibilitySensor>
   )
 }
 
